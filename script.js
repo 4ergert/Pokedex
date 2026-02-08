9cd39947de53f861934c167c3f3f54675993b128
J.Hergert <julian.konken@gmx.net>
2026-02-08 12:04:13 +0100

    capitalization for names

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
+let preLoadCase = [];
+let amountOfPokemons = 30
+
 async function fetchDataJason() {
-  let response = await fetch("https://pokeapi.co/api/v2/pokemon/1");
-  let responseAsJson = await response.json();
-  console.log(responseAsJson);
-  document.getElementById('content').innerHTML = `<h1>${responseAsJson.name}</h1><img src="${responseAsJson.sprites.front_default}" alt="">`
+  for (let APIindex = 1; APIindex <= amountOfPokemons; APIindex++) {
+    let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${APIindex}`);
+    let responseAsJson = await response.json();
+    preLoadCase.push(responseAsJson)
+  }
+  
+  for (let preLoadCaseIndex = 0; preLoadCaseIndex < preLoadCase.length; preLoadCaseIndex++) {
+    document.getElementById('content').innerHTML += getCardTemplate(preLoadCaseIndex);
+  }
+
+}
+
+function getCardTemplate(index) {
+
+  let name = preLoadCase[index].name.replace(/^./, char => char.toUpperCase());
+
+  return ` 
+    <section class="card">
+      <h1>${name}</h1>
+      <img src="${preLoadCase[index].sprites.other.dream_world.front_default}" alt="">
+    </section>
+  `
 }
