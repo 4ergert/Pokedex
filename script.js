5eff2d95ccfe5bec49e3f983a1414e32f7180da1
J.Hergert <julian.konken@gmx.net>
2026-02-08 15:28:40 +0100

    add pokemon types with different background colors

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let preLoadCase = [];
-let amountOfPokemons = 30
+let amountOfPokemons = 9
 
 async function fetchDataJason() {
   for (let APIindex = 1; APIindex <= amountOfPokemons; APIindex++) {
@@ -7,21 +7,28 @@ async function fetchDataJason() {
     let responseAsJson = await response.json();
     preLoadCase.push(responseAsJson)
   }
-  
+
   for (let preLoadCaseIndex = 0; preLoadCaseIndex < preLoadCase.length; preLoadCaseIndex++) {
     document.getElementById('content').innerHTML += getCardTemplate(preLoadCaseIndex);
+    if (preLoadCase[preLoadCaseIndex].types.length > 1) {
+      document.getElementById(`types_${preLoadCaseIndex}`).innerHTML += `
+        <p class="type_two">${preLoadCase[preLoadCaseIndex].types[1].type.name}</p>
+      `
+    }
   }
-
+  
 }
 
 function getCardTemplate(index) {
-
-  let name = preLoadCase[index].name.replace(/^./, char => char.toUpperCase());
+  let pokemoName = preLoadCase[index].name.replace(/^./, char => char.toUpperCase());
 
   return ` 
-    <section class="card">
-      <h1>${name}</h1>
+    <section class="card bg_${preLoadCase[index].types[0].type.name}">
+      <h1>${pokemoName}</h1>
       <img src="${preLoadCase[index].sprites.other.dream_world.front_default}" alt="">
+      <div class="types" id="types_${index}">
+        <p class="type_one">${preLoadCase[index].types[0].type.name}</p>
+      </div>
     </section>
   `
 }
